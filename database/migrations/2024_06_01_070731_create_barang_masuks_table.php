a5fd08e0343aed51ac6aafe6fe24bba2c8aaffbb
aldnnnz <sayabukanrobot999@gmail.com>
2024-06-01 15:01:44 +0700

    hello world

---
+++ database/migrations/2024_06_01_070731_create_barang_masuks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang_masuk', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_barang');
+            $table->foreign('id_barang')->references('id')->on('barang_mentah');
+            $table->date('tanggal');
+            $table->integer('jumlah');
+            $table->integer('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang_masuks');
+    }
+};
---
+++ database/migrations/2024_06_01_070816_create_transaksis_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->string('nama_pembeli');
+            $table->unsignedBigInteger('id_barang_keluar');
+            $table->foreign('id_barang_keluar')->references('id')->on('barang_keluar');
+            $table->integer('total_harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
